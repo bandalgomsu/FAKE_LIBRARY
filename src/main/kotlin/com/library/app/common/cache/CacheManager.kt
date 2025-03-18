1402b3876cd8daed6a979063ec2e079f73d2f7a8
반달곰수 <121839239+bandalgomsu@users.noreply.github.com>
2025-03-18 17:17:54 +0900

    feat : CacheManager Interface 구현

---
+++ src/main/kotlin/com/library/app/common/cache/CacheManager.kt
@@ -0,0 +1,27 @@
+package com.library.app.common.cache
+
+interface CacheManager {
+    suspend fun <T : Any> getOrLoad(
+        cacheType: CacheType,
+        key: String,
+        type: Class<T>,
+        loader: suspend () -> T
+    ): T
+
+    suspend fun <T : Any> evict(
+        cacheType: CacheType,
+        key: String,
+        type: Class<T>,
+        loader: suspend () -> T
+    ): T
+
+    suspend fun <T : Any> put(
+        cacheType: CacheType,
+        key: String,
+        type: Class<T>,
+        data: T
+    ) {
+
+    }
+    
+}
